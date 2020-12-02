714fbece1e69468bc3bc6913f602448e91ab0ec6
Samwise492 <anfamus2002@gmail.com>
2020-12-02 21:32:12 +0500

    added lights in scene, adjusted shadows in the first level, created rock platforms, rewrote "campfire_lighting" script on "light intensity" one

--- Assets/Scripts/LightIntensityVolume.cs
+++ Assets/Scripts/LightIntensityVolume.cs
@@ -5,10 +5,12 @@ using UnityEngine.Rendering;
 using UnityEngine;
 using UnityEngine.Experimental.Rendering.Universal;
 
-public class Campfire_lighting : MonoBehaviour
+public class LightIntensityVolume : MonoBehaviour
 {
     [SerializeField] Light2D lighting;
     [SerializeField] private float intensitySpeed;
+    [SerializeField] private float maxInstensity;
+    [SerializeField] private float minIntensity;
     private bool isIncreasing = true;
 
     void Update()
@@ -16,13 +18,13 @@ public class Campfire_lighting : MonoBehaviour
         if (isIncreasing)
         {
             lighting.intensity += intensitySpeed;
-            if (lighting.intensity >= 2)
+            if (lighting.intensity >= maxInstensity)
                 isIncreasing = false;
         }
         if (!isIncreasing)
         {
             lighting.intensity -= intensitySpeed;
-            if (lighting.intensity <= 1)
+            if (lighting.intensity <= minIntensity)
                 isIncreasing = true;
         }
     }
