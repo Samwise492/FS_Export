0090632acfccde449dac28822b8da1397a3de277
Samwise492 <anfamus2002@gmail.com>
2020-12-16 20:00:48 +0500

    was making level design (1st level)

---
+++ Assets/Scripts/TilemapTransparencyController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TilemapTransparencyController : MonoBehaviour
+{
+    [SerializeField] private Tilemap tilemap;
+    [SerializeField] private Trigger triggerPoint;
+
+    private void Update()
+    {
+        if (triggerPoint.isTrigger)
+            RevealTilemap();
+    }
+
+    public void RevealTilemap()
+    {
+        float transparency = tilemap.GetComponent<Color>().a;
+        transparency = 50;
+    }
+}
