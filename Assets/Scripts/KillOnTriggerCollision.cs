e07b6c71217e198fd31ca94a19b65ac4204cfc15
Samwise492 <anfamus2002@gmail.com>
2021-03-25 21:47:16 +0500

    added decorations in the 3rd level

---
+++ Assets/Scripts/KillOnTriggerCollision.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillOnTriggerCollision : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Debug.Log("caught");
+            var health = GameManager.Instance.healthContainer[col.gameObject];
+            Debug.Log("was " + health.CurrentHealth);
+            health.CurrentHealth -= 500;
+            Debug.Log("and now is " + health.CurrentHealth);
+        }
+            
+    }
+}
